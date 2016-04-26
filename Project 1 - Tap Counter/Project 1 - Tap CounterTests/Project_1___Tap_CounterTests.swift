fa640892aed5ca2ad059e17ddcca9d764a18e732
AlanYen <alanyen.mobile@gmail.com>
2016-04-26 12:19:30 +0800

    Add Project 1

---
+++ Project 1 - Tap Counter/Project 1 - Tap CounterTests/Project_1___Tap_CounterTests.swift
@@ -0,0 +1,36 @@
+//
+//  Project_1___Tap_CounterTests.swift
+//  Project 1 - Tap CounterTests
+//
+//  Created by AlanYen on 2016/4/26.
+//  Copyright © 2016年 17Life. All rights reserved.
+//
+
+import XCTest
+@testable import Project_1___Tap_Counter
+
+class Project_1___Tap_CounterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
